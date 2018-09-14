add6fe5d67dd372ad6f8e8355032ee4df46a970d
jainebudke <jainebudke@hotmail.com>
2018-09-14 14:55:58 -0300

    register page - ok

--- src/main/java/br/com/adapt/auth/RegisterController.java
+++ src/main/java/br/com/adapt/auth/RegisterController.java
@@ -1,10 +1,14 @@
 package br.com.adapt.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class RegisterController {
 
-	@GetMapping("/register")
+	@GetMapping("/user-register")
 	public String register() {
         return "auth/register";
     }
