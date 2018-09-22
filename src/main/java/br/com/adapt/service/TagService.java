5a723f5b2680dba81b0949e590a70088e3e03018
Mayra Azevedo <mayradazevedo@gmail.com>
2018-09-22 17:00:06 -0300

    init tag service

--- src/main/java/br/com/adapt/service/TagService.java
+++ src/main/java/br/com/adapt/service/TagService.java
@@ -3,10 +3,25 @@
  */
 package br.com.adapt.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.adapt.repository.TagRepository;
+import br.com.adapt.model.Tag;
+
 /**
  * @author mayra
  *
  */
+@Service
+@Transactional(readOnly = true)
 public class TagService {
+	@Autowired
+	private TagRepository tagRepository;
+	
+	
 
 }
