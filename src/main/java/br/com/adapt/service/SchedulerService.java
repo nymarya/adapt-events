a5826ad7e86ee4798c899108577a5b75d4928420
Mayra Azevedo <mayradazevedo@gmail.com>
2018-09-23 11:37:42 -0300

    set scheduler to user

--- src/main/java/br/com/adapt/service/SchedulerService.java
+++ src/main/java/br/com/adapt/service/SchedulerService.java
@@ -3,9 +3,34 @@
  */
 package br.com.adapt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.adapt.model.Scheduler;
+import br.com.adapt.model.User;
+import br.com.adapt.repository.SchedulerRepository;
+
 /**
  * @author mayra
  *
  */
+@Service
+@Transactional(readOnly = true)
 public class SchedulerService {
+	@Autowired
+	private SchedulerRepository schedulerRepository;
+	
+	/**
+	 * Ao criar um usuário, deve ser criado um 
+	 * scheduler paar ele
+	 * @param user
+	 * @return
+	 */
+	@Transactional(readOnly = false)
+	public Scheduler save(User user) {
+		final Scheduler scheduler = new Scheduler();
+		scheduler.setUser(user);
+		return schedulerRepository.save(scheduler);
+	}
 }
